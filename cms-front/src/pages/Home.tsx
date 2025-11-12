2f0bcb3a958d15ed6de164cfe7a95b39ff82d3da
Florencia Pons <ponsmariaflorencia@gmail.com>
2025-11-12 00:00:56 -0300

    feat: initial configuration of Router for navigate (#8)

---
+++ cms-front/src/pages/Home.tsx
@@ -0,0 +1,14 @@
+function Home() {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Bienvenidos a testimonials cms 🚀
+      </h1>
+      <p className="mt-4 text-gray-700">
+        BrowserRouter activado. Siiiii!!!!!
+      </p>
+    </div>
+  );
+}
+
+export default Home;
