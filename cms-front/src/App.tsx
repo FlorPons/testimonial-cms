46cf19746b87abbf73264f804bd4451474057ed6
Florencia Pons <ponsmariaflorencia@gmail.com>
2025-11-11 23:58:52 -0300

    feat: base structure of the application with Vite and React (#7)

---
+++ cms-front/src/App.tsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline text-blue-600">
+      Hola mundooooooooo! 🚀
+    </h1>
+  );
+}
+
+export default App;
---
+++ cms-front/src/main.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/react-query";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ToastContainer />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
